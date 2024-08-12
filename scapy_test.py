c04f98dd1137c161214dfaf9950a4f1417e320da
zahin <xahin96@gmail.com>
2024-08-11 23:42:31 -0400

    all demo code added

--- scapy_test.py
+++ scapy_test.py
@@ -3,7 +3,28 @@ from scapy.packet import ls
 
 from scapy.all import *
 
-iph = IP(src="10.0.2.4", dst="10.10.10.10")
 
-# Show the fields of the IP header
-ls(IP())
+# Define a callback function to handle sniffed packets
+def packet_callback(packet):
+    print("Sniffed Packet:")
+    packet.show()
+
+    # Example: Spoof a response packet (for demonstration purposes only)
+    if packet.haslayer(IP) and packet.haslayer(ICMP):
+        # Craft a spoofed ICMP Echo Reply packet
+        ip = IP(src=packet[IP].dst, dst=packet[IP].src)
+        icmp = ICMP(type=0, id=packet[ICMP].id, seq=packet[ICMP].seq)  # Echo Reply
+        spoofed_packet = ip / icmp
+        print("Sending Spoofed Packet:")
+        send(spoofed_packet)
+
+
+# Sniff packets on the network
+def start_sniffing(interface):
+    print(f"Starting to sniff on interface {interface}...")
+    sniff(iface=interface, prn=packet_callback, filter="icmp", store=0)
+
+
+# Specify the network interface to sniff on (e.g., 'eth0', 'wlan0', etc.)
+interface = 'en0'  # Change this to your network interface
+start_sniffing(interface)
